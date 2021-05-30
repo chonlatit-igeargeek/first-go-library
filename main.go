74d6d7086b79f57ea011625b439294c0c4e6774c
chonlatit inkaew <>
2021-05-30 16:05:42 +0700

    Update package

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package greetings
 import "fmt"
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	fmt.Println(fmt.Sprintf("Hi, %v. Welcome!", name))
 }
